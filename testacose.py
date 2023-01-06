11cf4b72ce9f25114f4b789ebb9f5660ed036e2c
Balbus95 <mariobalbi95@libero.it>
2023-01-06 03:29:14 +0100

    add log

--- testacose.py
+++ testacose.py
@@ -1,15 +1,19 @@
-for i in range(10):
-    def f1():
-        print(f"f1 {i}")
-        return 1
-    def f2():
-        print(f"f2 {i}")
-        return 0
-    def main():
-        f1()
-        f2()
-        print(f"main {i}")
-        return 2
+with open('filename.txt', 'w') as f:    
+    print("ggg",file=f)
+    
+    
+    # for i in range(10):
+    #     def f1():
+    #         print(f"f1 {i}")
+    #         return 1
+    #     def f2():
+    #         print(f"f2 {i}")
+    #         return 0
+    #     def main():
+    #         f1()
+    #         f2()
+    #         print(f"main {i}")
+    #         return 2
 
-    if __name__ == "__main__":
-        main()
+        # if __name__ == "__main__":
+        #     main()
