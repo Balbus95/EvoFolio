fe6ba9022a973efb50dc4adadc3f50a58df981ca
Balbus95 <43786593+Balbus95@users.noreply.github.com>
2023-01-10 11:55:49 +0100

    add elite

--- final.py
+++ final.py
@@ -17,18 +17,13 @@ with open('terminalout.txt', 'w') as term, open('log.txt', 'w') as logb:
     def isWindows():
         return os.name=="nt"
 
+
     ABSPATH=os.path.dirname(os.path.abspath(__file__))
 
     if(isWindows()): 
         PATHCSVFOLDER= ABSPATH+"\\stock\\WEEK" #path per windows
     else: PATHCSVFOLDER= ABSPATH+"/stock/WEEK" #path per unix
-
-    BOUND_LOW, BOUND_UP = 1, 10 # 0,10
-    NDIM = 4 #dimensione singola tupla default 30 # lunghezza portafoglio (numero di azioni disponibili)
-
-    MU = 24 #generazione tuple population, deve essere multiplo di 4 (Dimensione popolazione)
-
-
+  
     def genstockdf():
         stockdf=[]
         stocknames=[] 
@@ -40,6 +35,15 @@ with open('terminalout.txt', 'w') as term, open('log.txt', 'w') as logb:
             stockdf.append(df)
             i+=1
         return (stockdf,stocknames)
+
+    def maxazione(stockdf):
+        maxcost=0
+        for i in range(len(stockdf)):
+            for row in range(1,len(stockdf[0])+1):
+                cost = stockdf[i]["Close"][row-1]
+                if cost > maxcost:
+                    maxcost=cost
+        return maxcost
 
     def printpop(pop):
         print(f"{len(pop)} - ", end='',file=term)
@@ -64,12 +68,12 @@ with open('terminalout.txt', 'w') as term, open('log.txt', 'w') as logb:
         # print(f"avgtotal: {avgtotal}")
         return avgtotal
 
-    def contazeri(ind):
+    def contanonzeri(ind):
         count=0
         for num in ind:
             if(num==0):
                 count+=1
-        return count
+        return len(ind)-count
 
     def uniform(low, up, size=None): #creazione popolazione (funzione base)
         try:
@@ -80,16 +84,22 @@ with open('terminalout.txt', 'w') as term, open('log.txt', 'w') as logb:
     creator.create("FitnessMulti", base.Fitness, weights=(-1.0, 1.0))
     creator.create("Individual", array.array, typecode='d', fitness=creator.FitnessMulti)
 
+    stockdf,stocknames = genstockdf()
+    comb=combinator(len(stockdf))
+    maxtime=len(stockdf[0])
+   
+    BUDG = 3000
+    BOUND_LOW, BOUND_UP = 0, int(BUDG/maxazione(stockdf)) # 0,10
+    NDIM = 4 #dimensione singola tupla default 30 # lunghezza portafoglio (numero di azioni disponibili)
+
+    MU = 100 #generazione tuple population, deve essere multiplo di 4 (Dimensione popolazione)
+    
     random.seed()
     toolbox = base.Toolbox()
     toolbox.register("attr_float", uniform, BOUND_LOW, BOUND_UP, NDIM) #genera numeri
     toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.attr_float) #crea individui con attr_float
     toolbox.register("population", tools.initRepeat, list, toolbox.individual) #ripete funzione individual
     pop = toolbox.population(n=MU)
-
-    stockdf,stocknames = genstockdf()
-    comb=combinator(len(stockdf))
-    maxtime=len(stockdf[0])
 
     print(f"STOCK NAMES: {stocknames}",file=term)
     print(f"LISTA NOMI == DA AZIONI == STOCK AZIONI ({len(stocknames)} == {len(pop[0])} == {len(stockdf)})",file=term)
@@ -104,7 +114,7 @@ with open('terminalout.txt', 'w') as term, open('log.txt', 'w') as logb:
 
     for tempo in range(1,maxtime+1): #arriva alla riga del csv time-1 min=1 max 153 per WEEK 738 per DAY (NUMERO DI RIGHE DA PRENDERE)
         
-        # time.sleep(1)
+        time.sleep(1)
 
         def myfitness(ind):
             listvar=[]
@@ -136,7 +146,7 @@ with open('terminalout.txt', 'w') as term, open('log.txt', 'w') as logb:
         toolbox.register("select", tools.selNSGA2) # funzione di selection
 
         def main():
-
+            
             global pop
             
             if(len(stocknames)==len(pop[0])==len(stockdf)):
@@ -153,9 +163,6 @@ with open('terminalout.txt', 'w') as term, open('log.txt', 'w') as logb:
                 # ind=[4,1]
                 # mid=middle(stockdf,ind)
                 # print(mid)      
-
-                maxcost=maxazione(stockdf)
-                print(maxcost)
 
                 pop,logbook =nsga2(pop)
                 
@@ -179,10 +186,12 @@ with open('terminalout.txt', 'w') as term, open('log.txt', 'w') as logb:
 
             NGEN = 5 #numero generazioni
             CXPB = 0.9 # probability of mating each individual at each generation 
-            MAXZERI= 4 
-            SELPARAM= 1.0 # 0.8
-            TOURNPARAM= 1.0 # 0.9
-            BUDG = 10000
+            MAXAZIONI= 4 
+            MINAZIONI= 2
+            SELPARAM= 0.8 # 0.8
+            TOURNPARAM= 0.9 # 0.9
+            ELITEPARAM=0.5
+            PREF=[0,1,0,0]
 
 
             stats = tools.Statistics(lambda ind: ind.fitness.values)
@@ -206,13 +215,9 @@ with open('terminalout.txt', 'w') as term, open('log.txt', 'w') as logb:
 
             print('\n\n\t$$$ Prima di contazero e middle ',end='',file=term)
             printpop(pop)
-            pop = [ind for ind in pop if contazeri(ind)<=MAXZERI] #cancella individui che hanno più di MAXZERI azioni a 0
+            pop = [ind for ind in pop if (contanonzeri(ind)<=MAXAZIONI and contanonzeri(ind)>=MINAZIONI)] #cancella individui che hanno più di MAXZERI azioni a 0
             pop = [ind for ind in pop if middle(stockdf,ind)<=BUDG] #cancella individui che hanno speso più di BUDG
-            
-            if (len(pop)/4)==int(len(pop)/4):
-                print(f"{len(pop)} è multiplo di 4")
-            else: print(f"{len(pop)} NON è multiplo di 4")
-            
+
             print('\n\n\t$$$ dopo contazero e middle ',end='',file=term)
             printpop(pop)
 
@@ -242,20 +247,36 @@ with open('terminalout.txt', 'w') as term, open('log.txt', 'w') as logb:
                 print(f'\n\t\tdop gen {gen}: ',end='',file=term)
                 printpop(pop)
 
+
+                elite=genelite(pop,PREF)
+                elite = [toolbox.clone(ind) for ind in elite]
+
                 # Vary the population            
                 offspring = tools.selTournamentDCD(pop, int(len(pop)*TOURNPARAM))
                 offspring = [toolbox.clone(ind) for ind in offspring]
+
                 
                 for ind1, ind2 in zip(offspring[::2], offspring[1::2]):
 
                     if random.random() <= CXPB:
                         toolbox.mate(ind1, ind2)
-
+                    
                     toolbox.mutate(ind1)
                     toolbox.mutate(ind2)
                     del ind1.fitness.values, ind2.fitness.values
 
-                offspring = [ind for ind in offspring if contazeri(ind)<=MAXZERI] #cancella individui che hanno maxzeri azioni a 0
+                
+                for ind1 in elite:
+                    for ind2 in offspring:
+
+                        if random.random() <= ELITEPARAM:
+                            toolbox.mate(ind1, ind2)
+    
+                        toolbox.mutate(ind1)
+                        toolbox.mutate(ind2)
+                        del ind1.fitness.values, ind2.fitness.values
+                
+                offspring = [ind for ind in offspring if (contanonzeri(ind)<=MAXAZIONI and contanonzeri(ind)>=MINAZIONI)] #cancella individui che hanno maxzeri azioni a 0
                 offspring = [ind for ind in offspring if middle(stockdf,ind)<=BUDG] #cancella individua che hanno speso più di BUDG
             
 
@@ -332,7 +353,7 @@ with open('terminalout.txt', 'w') as term, open('log.txt', 'w') as logb:
             risk=(az1*var1)+(az2*var2)+(2*(az1*az2*cov)) 
             return risk
 
-        def grafico(valorimin,valorimid,valorimax):
+        def grafico(valorimid):
             date=pd.to_datetime(stockdf[0]["Date"]) 
             # plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=6))
             plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%b %Y')) # '%d-%m-%Y' ----- gca() get current axis, gcf() get current figure 
@@ -348,14 +369,17 @@ with open('terminalout.txt', 'w') as term, open('log.txt', 'w') as logb:
             # plt.gcf().autofmt_xdate()
             plt.show()   
 
-        def maxazione(stockdf):
-            maxcost=0
-            for i in range(len(stockdf)):
-                for row in range(1,len(stockdf[0])+1):
-                    cost = stockdf[i]["Close"][row-1]
-                    if cost > maxcost:
-                        maxcost=cost
-            return maxcost
+        def genelite(pop,pref):
+            indliked=[]
+            for i in range(len(pref)):
+                if(pref[i]==1):
+                    indmaxazioni=0
+                    for ind in pop:
+                        if(ind[i]>=indmaxazioni):
+                            indmaxazioni=ind[i]
+                            maxind=ind
+                    indliked.append(maxind)
+            return indliked
 
         if __name__ == "__main__":
             main()
